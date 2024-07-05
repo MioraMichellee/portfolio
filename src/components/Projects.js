de89b553c968f2fd926150b6d5e5282d16170bd5
MioraMichellee <mioramichelle25@gmail.com>
2024-07-05 18:42:41 -0300

    Add projects section

---
+++ src/components/Projects.js
@@ -0,0 +1,74 @@
+export const Projects = ()=>{
+    const projects =[
+        {
+            title:"",
+            description:"",
+            imgUrl:projImg,
+        },
+        {
+            title:"",
+            description:"",
+            imgUrl:projImg,
+        },
+        {
+            title:"",
+            description:"",
+            imgUrl:projImg,
+        },
+        {
+            title:"",
+            description:"",
+            imgUrl:projImg,
+        },
+        {
+            title:"",
+            description:"",
+            imgUrl:projImg,
+        },
+        {
+            title:"",
+            description:"",
+            imgUrl:projImg,
+        },
+        
+    ]
+        
+    return (
+        <section >
+            <Row>
+                <Col>
+                    <h2></h2>
+                    <p></p>
+                    <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" defaultActiveKey="/home">
+                        <Nav.Item>
+                            <Nav.Link eventKey="first">Tab one </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="second">Tab two</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="third">Tab three</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                    <Tab.Content>
+                        <Tab.Pane eventKey="first">
+                            <Row>
+                                {
+                                    projects.map((projects,index)=>{
+                                        return(
+                                            <p>{projects.title}</p>
+                                        )
+                                    })
+                                }
+                            </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="second"> Loren Ipsum </Tab.Pane>
+                        <Tab.Pane eventKey="third"> Loren Ipsum </Tab.Pane>
+                    </Tab.Content>
+                    </Tab.Container>
+                </Col>
+            </Row>
+        </section>
+    )
+}
