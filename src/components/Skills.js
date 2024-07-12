6dd5e0a2519b2d31b66d3f798fe211917ff0188b
MioraMichellee <mioramichelle25@gmail.com>
2024-07-12 17:35:57 -0300

    fix: add valid alt to Skills img

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -38,19 +38,19 @@ import colorSharp from "../assets/img/color-sharp.png"
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic</p>
                             <Carousel responsive={responsive} infinite = {true} className="skill-slider">
                                 <div className="item">
-                                    <img src={meter1} alt="image"/>
+                                    <img src={meter1} alt="image1"/>
                                     <h5>Web Development</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter2} alt="image"/>
+                                    <img src={meter2} alt="image2"/>
                                     <h5>Brand identity</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter3} alt="image"/>
+                                    <img src={meter3} alt="image3"/>
                                     <h5>Web Development</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter1} alt="image"/>
+                                    <img src={meter1} alt="image4"/>
                                     <h5>Web Development</h5>
                                 </div>
                             </Carousel>
