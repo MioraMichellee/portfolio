fc71a212420f192336526ac0c746429084dceeca
MioraMichellee <mioramichelle25@gmail.com>
2024-07-12 18:06:05 -0300

    it's a test for the CI CD

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -54,7 +54,7 @@ export const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">Welcome to my portfolio</span>
+                        {/* <span className="tagline">Welcome to my portfolio</span> */}
                         <h1>{"Hi I'm a MioraMichelle "} <span className="warp">{text}</span></h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic</p>
                         <button onClick={()=>console.log('CONNECT')}>Let's connect<ArrowRightCircle size={25}/></button>
