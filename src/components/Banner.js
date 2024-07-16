bf10aabb6f3a31005e961d5a5e6f0552d31e6fb5
MioraMichellee <mioramichelle25@gmail.com>
2024-07-16 10:19:50 -0300

    style:"Add animation"

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,8 @@ import { ArrowRightCircle } from "react-bootstrap-icons";
 import mercury from "../assets/img/mercury.png"
 import saturne from "../assets/img/saturne.png"
 import headerImg from "../assets/img/mi-astronaute.png"
-
+import TrackVisibility from "react-on-screen";
+import 'animate.css'
 
 export const Banner = () => {
 
@@ -46,21 +47,27 @@ export const Banner = () => {
 
     return(
         <section className="banner" id="home">
-            <img src={mercury} className="absolute mercury-img" alt="mercury"/>
+            <img src={mercury} className="fixed mercury-img" alt="mercury"/>
             <div className="rotate">
-            <img src={saturne} className="absolute saturne-img" alt="absolute"/>
+           
             </div>
             
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">Welcome to my portfolio</span>
-                        <h1>{"Hi I'm a MioraMichelle "} <span className="warp">{text}</span></h1>
-                        <p>Embark on a journey through my creations and technical explorations. Just as astronauts explore the vast unknown of space, I delve into the infinite possibilities of coding. As a third-year student at ENI Fianarantsoa, I am passionate about innovation. My journey is marked by various projects, each providing a valuable opportunity to learn and perfect my skills.</p>
-                        <button onClick={()=>console.log('CONNECT')}>Let's connect<ArrowRightCircle size={25}/></button>
+                    <TrackVisibility>
+                        {({ isVisible }) =>
+                            <div className={isVisible ? "animate__animated animate__fadeIn animate__slow-5 animate__delay-0.01s" : ""}>
+                                <span className="tagline">Welcome to my portfolio</span>
+                                <h1>Hi I'm a Miora Michelle <span className="warp">{text}</span></h1>
+                                <p>Embark on a journey through my creations and technical explorations. Just as astronauts explore the vast unknown of space, I delve into the infinite possibilities of coding. As a third-year student at ENI Fianarantsoa, I am passionate about innovation. My journey is marked by various projects, each providing a valuable opportunity to learn and perfect my skills.</p>
+                                <button onClick={()=>console.log('CONNECT')}>Let's connect<ArrowRightCircle size={25}/></button>
+                            </div>}
+                        </TrackVisibility>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg} alt="headerImg"/>
+                        <img src={saturne} className="fixed saturne-img" alt="absolute"/>
                     </Col>
                 </Row>
             </Container>
